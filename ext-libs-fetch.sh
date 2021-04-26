dba4e7b902e2f1821260152a614e376d3c241aac
tmpfs <tmpfs-code@users.noreply.github.com>
2021-04-26 10:09:22 +0200

    update ext libs; use ponyfill instead of polyfill as suggested by the streamsaver

--- ext-libs-fetch.sh
+++ ext-libs-fetch.sh
@@ -12,14 +12,28 @@ which curl
 # This is used as a fallback for browser that are not currenly supporting window.ReadableStream.
 # Read more here: https://www.npmjs.com/package/web-streams-polyfill
 
-rm -f ./public/polyfill.min.js
+# rm -f ./public/polyfill.min.js
+# rm -f ./public/polyfill.min.js.map
+
+# curl --location \
+#   --max-redirs 3 \
+#   --tlsv1.3 \
+#   --connect-timeout 10 \
+#   --output ./public/polyfill.min.js \
+#   https://unpkg.com/web-streams-polyfill/dist/polyfill.min.js 
+
+# "[...] It's better to load the ponyfill instead of the polyfill and override the existing implementation [...]"
+# see https://www.npmjs.com/package/streamsaver
+
+rm -f ./public/ponyfill.min.js
+rm -f ./public/ponyfill.min.js.map
 
 curl --location \
   --max-redirs 3 \
-  --tlsv1.3 \
+  --tlsv1.2 \
   --connect-timeout 10 \
-  --output ./public/polyfill.min.js \
-  https://unpkg.com/web-streams-polyfill/dist/polyfill.min.js 
+  --output ./public/ponyfill.min.js \
+  https://cdn.jsdelivr.net/npm/web-streams-polyfill/dist/ponyfill.min.js
 
 
 # Fetch https://unpkg.com/web-streams-polyfill/dist/polyfill.min.js and save it to public/
@@ -42,3 +56,5 @@ curl --tlsv1.2 \
 
 echo
 echo "PLEASE UPDATE ext-libs-checksums.txt"
+# To update ext-libs-checksums.txt use the command:
+# sha256sum public/ponyfill.min.js public/sw.js public/mitm.html > ./ext-libs-checksums.txt 
